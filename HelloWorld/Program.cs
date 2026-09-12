ec254534841d287c2035697deee53e6e33493a19
Korand-py <MSOkishev@yandex.ru>
2026-09-12 11:11:52 +0300

    Step 7.5

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿int age = 20;
+double gpa = 4.5;
+bool isStudent = true;
+string name = "Макс";
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa);
+Console.WriteLine(isStudent);
