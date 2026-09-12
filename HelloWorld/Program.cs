057cde0d6918d9c9792db5ab8983f5ce6bffedf2
Korand-py <MSOkishev@yandex.ru>
2026-09-12 11:16:13 +0300

    Hello Name

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,18 @@
-﻿int age = 20;
-double gpa = 4.5;
-bool isStudent = true;
-string name = "Макс";
-Console.WriteLine(name);
-Console.WriteLine(age);
-Console.WriteLine(gpa);
-Console.WriteLine(isStudent);
+﻿// int age = 20;
+// double gpa = 4.5;
+// bool isStudent = true;
+// string name = "Макс";
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(isStudent);
+
+// var city = "Москва";
+// var year = 2026;
+// var pi = 3.14159;
+// var isActive = true;
+
+string myName = "Иван";
+int myAge = 20;
+string myGroup = "ИСП-242";
+Console.WriteLine($"Меня зовут {myName}, мне {myAge} лет, я учусь в группе {myGroup}");
