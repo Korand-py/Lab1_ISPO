60d7777ed4851c8e84f43a8f656b6ae301c2242a
korand-py <MSOkishev@yandex.ru>
2026-09-19 20:37:02 +0300

    Step 9: complete student questionnaire program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -54,38 +54,73 @@
 // string obj = Console.ReadLine();
 // Console.WriteLine($"{fn} любит {obj}");
 
-int a = 15;
-int b = 4;
-Console.WriteLine($"Сумма: {a + b}");
-Console.WriteLine($"Разность: {a - b}");
-Console.WriteLine($"Произведение: {a * b}");
-Console.WriteLine($"Частное (int): {a / b}");
-Console.WriteLine($"Остаток: {a % b}");
+// int a = 15;
+// int b = 4;
+// Console.WriteLine($"Сумма: {a + b}");
+// Console.WriteLine($"Разность: {a - b}");
+// Console.WriteLine($"Произведение: {a * b}");
+// Console.WriteLine($"Частное (int): {a / b}");
+// Console.WriteLine($"Остаток: {a % b}");
 
-double res = (double)a / b;
-Console.WriteLine($"Частное (double): {res}");
+// double res = (double)a / b;
+// Console.WriteLine($"Частное (double): {res}");
 
-Console.WriteLine(Math.Abs(-5));
-Console.WriteLine(Math.Pow(2, 10));
-Console.WriteLine(Math.Sqrt(144));
-Console.WriteLine(Math.Max(10, 25));
-Console.WriteLine(Math.Min(10, 25));
-Console.WriteLine(Math.Round(3.567, 2));
+// Console.WriteLine(Math.Abs(-5));
+// Console.WriteLine(Math.Pow(2, 10));
+// Console.WriteLine(Math.Sqrt(144));
+// Console.WriteLine(Math.Max(10, 25));
+// Console.WriteLine(Math.Min(10, 25));
+// Console.WriteLine(Math.Round(3.567, 2));
 
-Console.WriteLine("Калькулятор");
-Console.Write("Введите первое число: ");
-double num1 = double.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-double num2 = double.Parse(Console.ReadLine());
-Console.WriteLine($"Сумма: {num1 + num2}");
-Console.WriteLine($"Разность: {num1 - num2}");
-Console.WriteLine($"Произведение: {num1 * num2}");
-//
-if (num2 != 0)
-{
-    Console.WriteLine($"Частное: {num1 / num2}");
-} else
-{
-    Console.WriteLine("Деление на ноль невозможно!");
-}
+// Console.WriteLine("Калькулятор");
+// Console.Write("Введите первое число: ");
+// double num1 = double.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// double num2 = double.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма: {num1 + num2}");
+// Console.WriteLine($"Разность: {num1 - num2}");
+// Console.WriteLine($"Произведение: {num1 * num2}");
 
+// if (num2 != 0)
+// {
+//     Console.WriteLine($"Частное: {num1 / num2}");
+// } else
+// {
+//     Console.WriteLine("Деление на ноль невозможно!");
+// }
+
+using System.Collections.Concurrent;
+
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+
+Console.WriteLine("Добро пожаловать в анкету!");
+Console.WriteLine("Введите имя: ");
+String name = Console.ReadLine();
+Console.WriteLine("Введите фамилию: ");
+String surname = Console.ReadLine();
+Console.WriteLine("Введите группу: ");
+String group = Console.ReadLine();
+Console.WriteLine("Введите год рождения: ");
+int birthYear = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите ваш средний бал (4.5): ");
+double gpa = double.Parse(Console.ReadLine());
+
+int currentYear = 2026;
+int age = currentYear - birthYear;
+bool isExcellent = gpa >= 4.5;
+string status;
+if (isExcellent == true) status = "Отличник";
+else status = "Хорошист";
+
+Console.WriteLine("Ваша анкета");
+Console.WriteLine($"Имя: {name} {surname}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний бал: {gpa}");
+Console.WriteLine($"Статус: {status}");
+Console.WriteLine($"Лет до 30: {30 - age}");
+Console.WriteLine("Нажмите Enter для выхода...");
+Console.ReadLine();
